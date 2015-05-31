bd74ee6b798141f94a43add1bc2b3b66a34a526e
fengb <contact@fengb.info>
2015-05-30 19:01:15 -0500

    Refactored gulpfile for great justice

--- Gulpfile-es6.js
+++ Gulpfile-es6.js
@@ -2,44 +2,46 @@ let _ = require('lodash')
 let isparta = require('isparta')
 
 let gulp = require('gulp')
-let jshint = require('gulp-jshint')
-let mocha = require('gulp-mocha')
-let gprint = require('gulp-print')
-let istanbul = require('gulp-istanbul')
+_.extend(gulp, {
+  jshint:   require('gulp-jshint'),
+  mocha:    require('gulp-mocha'),
+  print:    require('gulp-print'),
+  istanbul: require('gulp-istanbul'),
+})
 
 let paths = {
-  meta: ['.jshintrc', '*.{js,json,es6}'],
-  lib:  ['lib/**/*.{js,es6}'],
-  test: ['test/**/*.{js,es6}'],
+  meta: ['.jshintrc', '*.{js,json}'],
+  lib:  ['lib/**/*.js'],
+  test: ['test/**/*.js'],
 }
 paths.all = _(paths).values().flatten().value()
 
-gulp.task('files', () => {
-  return gulp.src(paths.all)
-        .pipe(gprint())
-})
+gulp.task('files', () =>
+  gulp.src(paths.all)
+    .pipe(gulp.print())
+)
 
-gulp.task('lint', () => {
-  return gulp.src(paths.all)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-})
+gulp.task('lint', () =>
+  gulp.src(paths.all)
+    .pipe(gulp.jshint())
+    .pipe(gulp.jshint.reporter('default'))
+)
 
-gulp.task('mocha', () => {
-  return gulp.src(paths.lib)
-        .pipe(istanbul({ instrumenter: isparta.Instrumenter }))
-        .pipe(istanbul.hookRequire())
-        .on('finish', () => {
-          return gulp.src(paths.test)
-                .pipe(mocha({ reporter: 'dot' }))
-                .pipe(istanbul.writeReports({ reporters: ['lcov'] }))
-        })
-})
+gulp.task('mocha', () =>
+  gulp.src(paths.lib)
+    .pipe(gulp.istanbul({ instrumenter: isparta.Instrumenter }))
+    .pipe(gulp.istanbul.hookRequire())
+    .on('finish', () =>
+      gulp.src(paths.test)
+        .pipe(gulp.mocha({ reporter: 'dot' }))
+        .pipe(gulp.istanbul.writeReports({ reporters: ['lcov'] }))
+    )
+)
 
 gulp.task('test', ['lint', 'mocha'])
 
-gulp.task('watch-test', () => {
+gulp.task('watch-test', () =>
   gulp.watch(paths.all, ['lint', 'test'])
-})
+)
 
 gulp.task('default', ['test'])
