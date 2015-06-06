6123065b36d4ae67757f24b530edd727b0aee65d
fengb <contact@fengb.info>
2015-06-06 15:46:59 -0500

    Optional coverage since it keeps bombing out

--- Gulpfile-es6.js
+++ Gulpfile-es6.js
@@ -30,18 +30,32 @@ gulp.task('lint', () =>
     .pipe(gulp.jshint.reporter('default'))
 )
 
-gulp.task('_istanbul', () =>
+gulp.task('mocha', () =>
+  gulp.src(PATH.test)
+    .pipe(gulp.mocha({ reporter: 'dot' }))
+)
+
+gulp.task('istanbul-init', () =>
   gulp.src(PATH.lib)
     .pipe(gulp.istanbul({ instrumenter: isparta.Instrumenter }))
     .pipe(gulp.istanbul.hookRequire())
 )
 
-gulp.task('mocha', ['_istanbul'], () =>
+gulp.task('mocha-coverage', ['istanbul-init'], () =>
   gulp.src(PATH.test)
     .pipe(gulp.plumber())
     .pipe(gulp.mocha({ reporter: 'dot' }))
     .pipe(gulp.istanbul.writeReports({ reporters: ['lcov'] }))
 )
+
+gulp.task('mocha', (done) => {
+  if(process.env.TC_COVERAGE){
+    gulp.runSequence('mocha-coverage', done)
+  } else {
+    return gulp.src(PATH.test)
+      .pipe(gulp.mocha({ reporter: 'dot' }))
+  }
+})
 
 gulp.task('test', (done) => {
   gulp.runSequence('lint', 'mocha', done)
