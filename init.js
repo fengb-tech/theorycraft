adb771e0afcd8047b907779623e049a7296231d0
fengb <contact@fengb.info>
2015-09-09 22:33:45 -0500

    Blacklist babel features

--- init.js
+++ init.js
@@ -1,4 +1,14 @@
-require('babel/register')
+require('babel/register')({
+  blacklist: [
+    'es6.classes',
+    'es6.forOf',
+    'es6.objectSuper',
+    'es6.properties.computed',
+    'es6.properties.shorthand',
+    'es6.spec.symbols',
+    'regenerator',
+  ]
+})
 
 if(process.env.NODE_PATH){
   process.env.NODE_PATH += `:${__dirname}`
