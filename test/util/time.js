b4ae94e8d9d1e0491a77f382d620bd07f72da6b2
fengb <contact@fengb.info>
2015-09-16 01:06:57 -0500

    Added time function

--- test/util/time.js
+++ test/util/time.js
@@ -3,6 +3,15 @@ const { expect } = require('test/support')
 const time = require('lib/util/time')
 
 describe('lib/util/time', () => {
+  describe('time()', () => {
+    it('converts to milliseconds', () => {
+      expect(time({ ms: 8 })).to.equal(8)
+      expect(time({ sec: 4 })).to.equal(4000)
+      expect(time({ min: 1 })).to.equal(60000)
+      expect(time({ min: 1, sec: 4, ms: 8 })).to.equal(64008)
+    })
+  })
+
   describe('setNow', () => {
     afterEach(() => {
       time.resetNow()
